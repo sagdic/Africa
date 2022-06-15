d59bcb5e718adfdb4109c99209121518e09faa13
Tayfun Sağdıç <30289904+sagdic@users.noreply.github.com>
2022-06-15 12:16:46 +0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by tayfun on 15.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
